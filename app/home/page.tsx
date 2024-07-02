3325fc502fb44a3ca1fd7b7c95f642c40325acdd
Abhishek Wilson <abhishek.wilson@amnhealthcare.com>
2024-07-03 04:58:40 +0530

    Initial commit

---
+++ app/home/page.tsx
@@ -0,0 +1,14 @@
+import Carousel from "@/components/Carousel";
+import Header from "@/components/Header";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <Carousel />
+    </>
+  );
+};
+
+export default HomePage;
